f93df8fb15df9053ed993267b3b4deb9962a8d30
Kris Zyp <kriszyp@gmail.com>
2012-01-23 12:21:42 -0700

    Add corresponding errors for 7XX status codes

--- errors.js
+++ errors.js
@@ -10,3 +10,43 @@ NotFoundError.prototype.code = 2;
 
 var PreconditionFailed = exports.PreconditionFailed = ErrorConstructor("PreconditionFailed", DatabaseError);
 PreconditionFailed.prototype.code = 3;
+
+var otherErrors = ["Meh", // following the standard HTTP status codes at https://github.com/joho/7XX-rfc
+"Unpossible",
+"KnownUnknowns",
+"UnknownUnknowns",
+"Tricky",
+"ThisLineShouldBeUnreachable",
+"ItWorksOnMyMachine",
+"ItsAFeatureNotABug",
+"Compiling",
+"AKittenDies",
+"IThoughtIKnewRegularExpressions",
+"YUNOWriteIntegrationTests",
+"IDontAlwaysTestMyCodeButWhenIDoIDoItInProduction",
+"MissedBallmerPeak",
+"ConfoundedByPonies",
+"ReservedForChuckNorris",
+"Hungover",
+"Stoned",
+"OverCaffeinated",
+"UnderCaffeinated",
+"JSConf",
+"Sober",
+"Drunk",
+"CachedForTooLong",
+"NotCachedLongEnough",
+"NotCachedAtAll",
+"WhyWasThisCached?",
+"ErrorOnTheException",
+"Coincidence",
+"OffByOneError",
+"OffByTooManyToCountError",
+"SomebodyElsesProblemOperations",
+"SomebodyElsesProblemQA",
+"SomebodyElsesProblemItWasACustomerRequestHonestly",
+"SomebodyElsesProblemManagementObviously",
+"TPSCoverSheetNotAttached"];
+otherErrors.forEach(function(name){
+	exports[name + "Error"] = ErrorConstructor(name + "Error");
+});
