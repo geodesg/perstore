fa8540ee9fa3d16cad08777ede00ead5bf20d1a2
Kris Zyp <kriszyp@gmail.com>
2013-11-17 19:51:57 -0700

    Support getResource in ringo, fixes #44

--- util/settings.js
+++ util/settings.js
@@ -12,15 +12,19 @@ try{
 	var settings = JSON.parse(read("local.json").toString("utf8"));
 }catch(e){
 	try{
-		settings = require("rc")("persvr",{
-			"processes": 2,
-			"port": 8082,
-			"repl": true,
-			"replPort": 5555,
-			"security":{
-			},
-			"dataFolder": "data"
-		});
+		if(typeof java == "object"){
+			settings = JSON.parse(getResource("local.json").content);
+		}else{
+			settings = require("rc")("persvr",{
+				"processes": 2,
+				"port": 8082,
+				"repl": true,
+				"replPort": 5555,
+				"security":{
+				},
+				"dataFolder": "data"
+			});
+		}
 	}catch(e2){
 		console.error("A local.json file could not be found or parsed, and rc was not available to load configuration settings", e);		
 	}
